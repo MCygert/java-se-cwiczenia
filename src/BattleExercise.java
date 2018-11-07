b2090ba1b30750380fa1c5431395f6d79343a4f4
mikolajCygert <miki@comsite.com.pl>
2018-11-07 16:58:25 +0100

    Not done BattleExercise probably homework

---
+++ src/BattleExercise.java
@@ -0,0 +1,15 @@
+public class BattleExercise {
+    public static void main(String[] args) {
+        //Inicjalizacja
+         String battle = "wpzd";
+         battle.toCharArray();
+
+
+         for (int i = 0; i<battle.length();i++ ){
+
+         }
+
+
+    }
+}
+//Stworzyc tabele liczb ktore beda imitiowac mape na ascciii kod, Nastepnie sprawdzac wszystkie i uzywac ascii kodu jaki indexu.
