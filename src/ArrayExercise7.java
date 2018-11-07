dd909b3a97278e2d58f3b1c943d8a2883e24610b
mikolajCygert <miki@comsite.com.pl>
2018-11-07 16:58:15 +0100

    Array example done

--- src/ArrayExercise7.java
+++ src/ArrayExercise7.java
@@ -1,19 +1,35 @@
 public class ArrayExercise7 {
     public static void main(String[] args) {
-         int[] tArray = {1,2,3,4,5};
-         int[] tempArray = new int[3];
-         final int x = 3;
-         //Wziecei kazdej liczby po kolei jako pierwsza
-         for (int i = 0; i<tArray.length; i++){
-             //Sprawdzanie z kazda kolejna liczba w tej samej tablicy
-             for (int j = 1; j<tArray.length; j++){
-                 if(tArray[i]+tArray[j] == x){
-                     //Usuniecie elemntu j z tablicy:dodanie liczby i do nowej tablicy
-                     
+        int[] tArray = {1, 2, 3, 4, 5};
+        int[] tempArray = new int[10];
+        final int x = 3;
+        int count = 0;
+        //Wziecei kazdej liczby po kolei jako pierwsza
+        for (int i = 0; i < tArray.length; i++) {
+            //Sprawdzanie z kazda kolejna liczba w tej samej tablicy
+            for (int j = 1; j < tArray.length; j++) {
+                if (tArray[i] + tArray[j] == x) {
+                    //Dodanie zlego elemntu do tymczasowej tablicy
+                    tempArray[i] = tArray[j];
+                    count++;
+                }
 
+            }
+        }
+        int[] result = new int[4 - count];
+        for (int i = 0; i < tArray.length; i++) {
+            if (tArray[i] == tempArray[0]) {
+                i -= 1;
+                continue;
+            }
+            result[i] = tArray[i];
 
-                 }
-             }
+        }
+        for (int i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
         }
     }
 }
+
+
+
