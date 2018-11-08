b025c9b935603c42184f186744c6f32a60c57083
mikolajCygert <miki@comsite.com.pl>
2018-11-08 13:04:08 +0100

    Changing exercises with functions

---
+++ src/Rearanging/AverageExercise.java
@@ -0,0 +1,22 @@
+package Rearanging;
+
+import java.util.Scanner;
+
+public class AverageExercise {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        int num1 = in.nextInt();
+        System.out.println("Podaj liczbe");
+        int num3 = in.nextInt();
+        System.out.println("Podaj liczbe");
+        int num2 = in.nextInt();
+
+        System.out.println("Srednia to: " + averageNumber(num1, num2, num3))
+    }
+
+    static double averageNumber(int number1, int number2, int number3) {
+        double medium = (number1 + number2 + number3) / 3;
+        return medium;
+    }
+}
---
+++ src/RecursionTest.java
@@ -0,0 +1,15 @@
+public class RecursionTest {
+    public static void main(String[] args) {
+        recursiveFunction(10);
+    }
+
+    //Funkcja rekurencyjna
+    static void recursiveFunction(int iterator) {
+        if (iterator > 0) {
+            System.out.println("Iterator value" + iterator);
+            //Odwolanie do funkcji samej siebie
+            recursiveFunction(iterator - 1);
+        }
+        System.out.println("Recursion is over");
+    }
+}
