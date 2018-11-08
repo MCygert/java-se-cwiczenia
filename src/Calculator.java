a99afe2cb2e021af7de53b8e67e4a733ce98c141
mikolajCygert <miki@comsite.com.pl>
2018-11-08 14:34:22 +0100

    Rearanged calulator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,3 @@
-import java.sql.SQLSyntaxErrorException;
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,41 +7,80 @@ public class Calculator {
         //Petla ktora podtrzymuje kalkulator
         while (conitnueProgram) {
             //Pobranie liczb
-            System.out.println("Podaj liczbe: ");
-            int num1 = in.nextInt();
-            System.out.println("Podaj druga liczbe: ");
-            int num2 = in.nextInt();
+            int num1 = readNumberFromConsole(1);
+            int num2 = readNumberFromConsole(2);
 
             //Pobranie rodzaju aplikacji
-            System.out.println("Podaj operacje");
-            System.out.println("+ dodaje");
-            System.out.println("- odejmuje");
-            System.out.println("* mnozy");
-            System.out.println("/ dzieli");
-            System.out.println("q konczy dzialanie");
+            printMenuOptions();
             String operation = in.next();
 
-            //Switch z operacjami kalkulator
-            switch (operation) {
-                case "+":
-                    System.out.println(num1 + num2);
-                    break;
-                case "-":
-                    System.out.println(num1 - num2);
-                    break;
-                case "*":
-                    System.out.println(num1 * num2);
-                    break;
-                case "/":
-                    System.out.println(num1 / num2);
-                    break;
-                case "q":
-                    conitnueProgram = false;
-                    break;
-                default:
-                    System.out.println("Operacja" + operation);
-            }
+            //Odwolanie do funkcji z Switchem z operacjami kalkulatora
+            conitnueProgram = shouldConitnueProgram(conitnueProgram, num1, num2, operation);
 
         }
     }
+
+    static boolean shouldConitnueProgram(boolean conitnueProgram, int num1, int num2, String operation) {
+        switch (operation) {
+            case "+":
+                add(num1, num2);
+                break;
+            case "-":
+                substracion(num1, num2);
+                break;
+            case "*":
+                multiplication(num1, num2);
+                break;
+            case "/":
+                substracion(num1, num2);
+                break;
+            case "q":
+                conitnueProgram = false;
+                break;
+            default:
+                System.out.println("Operacja" + operation);
+        }
+        return conitnueProgram;
+    }
+
+    static int readNumberFromConsole(int numberInOrder) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczeb: " + numberInOrder);
+        return scanner.nextInt();
+    }
+
+    static int add(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println(result);
+        return result;
+    }
+
+    static int substracion(int num1, int num2) {
+        int result = num1 - num2;
+        System.out.println(result);
+        return result;
+    }
+
+    static int multiplication(int num1, int num2) {
+        int result = num1 * num2;
+        System.out.println(result);
+        return result;
+    }
+
+    static int division(int num1, int num2) {
+        int result = num1 / num2;
+        System.out.println(result);
+        return result;
+    }
+
+    static void printMenuOptions() {
+        System.out.println("Podaj operacje");
+        System.out.println("+ dodaje");
+        System.out.println("- odejmuje");
+        System.out.println("* mnozy");
+        System.out.println("/ dzieli");
+        System.out.println("q konczy dzialanie");
+    }
+
+
 }
