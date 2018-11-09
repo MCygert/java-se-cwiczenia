711d6c94f83f50223954fd2b8f4d56985477e6d0
mikolajCygert <miki@comsite.com.pl>
2018-11-09 16:48:07 +0100

    Program java

---
+++ src/Program.java
@@ -0,0 +1,18 @@
+public class Program {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3};
+        System.out.println(findLargestNum(array));
+    }
+
+    public static int findLargestNum(int[] nums) {
+        int lowestNumber = nums[0];
+        for (int number : nums) {
+            if (number < lowestNumber) {
+                lowestNumber = number;
+            }
+
+        }
+
+        return lowestNumber;
+    }
+}
