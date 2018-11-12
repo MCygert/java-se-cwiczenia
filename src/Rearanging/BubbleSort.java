7ae0ecea9bb81ace18dc9ed8903cb15883b7d22a
mikolajCygert <miki@comsite.com.pl>
2018-11-12 16:57:49 +0100

    RightTriangle

---
+++ src/Rearanging/BubbleSort.java
@@ -0,0 +1,28 @@
+package Rearanging;
+
+public class BubbleSort {
+    private static int x;
+
+    public static void main(String[] args) {
+        int[] numberArray = {4, 5, 6, 2, 1, 7};
+
+    }
+
+    static int[] sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                int tmp = array[i];
+                array[i] = array[i + 1];
+                array[i] = tmp;
+
+            }
+        }
+        n = n - 1;
+        while (n > 1) {
+            break;
+        }
+
+        return array[];
+    }
+}
