234b78a3bbf5ea5fc86d5d054e653fda3705003e
mikolajCygert <miki@comsite.com.pl>
2018-11-13 11:19:20 +0100

    Getter and setters for rectangle

--- src/Objective/Rectangle.java
+++ src/Objective/Rectangle.java
@@ -4,16 +4,33 @@ public class Rectangle {
     private double a;
     private double b;
 
+    //Getters and Setters
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
     public Rectangle(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
-    double countArea() {
+    public double countArea() {
         return a * b;
     }
 
-    double countPerimeter() {
+    public double countPerimeter() {
         return (2 * a) + (2 * b);
     }
 }
