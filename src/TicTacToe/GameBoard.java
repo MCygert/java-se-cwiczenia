4a5ef7e7add673d9d50f81fa38b2467063aa1d93
mikolajCygert <miki@comsite.com.pl>
2018-11-14 16:25:15 +0100

    Gameboard objecit of TicTacToe

--- src/TicTacToe/GameBoard.java
+++ src/TicTacToe/GameBoard.java
@@ -1,33 +1,114 @@
+
 package TicTacToe;
 
 public class GameBoard {
-    private char[][] array;
+    private Mark[][] array;
 
 
     public GameBoard(int height) {
+        //If user gives too small gameboard it makes default 3x3
         if (height < 3) {
-            this.array = new char[3][3];
-            this.array = emptyGameBoard(array);
+            height = 3;
         }
-        this.array = new char[height][height];
-        this.array = emptyGameBoard(array);
+
+        this.array = new Mark[height][height];
+        emptyGameBoard();
     }
 
-    public char[][] getArray() {
+    public Mark[][] getArray() {
         return array;
     }
 
-    public void setArray(char[][] array) {
+    public void setArray(Mark[][] array) {
         this.array = array;
     }
 
-    private char[][] emptyGameBoard(char[][] gameBoard) {
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard.length; j++) {
-                gameBoard[i][j] = '?';
+    //Creating emptygameboard filled with enums Mark.EMPTY
+    private void  emptyGameBoard() {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = Mark.EMPTY;
             }
         }
-        return gameBoard;
+
     }
 
+
+    @Override
+    public String toString() {
+        String boardString = "";
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                switch (array[i][j]) {
+                    case O:
+                        boardString += 'O';
+                        break;
+                    case X:
+                        boardString += 'X';
+                        break;
+                    case EMPTY:
+                        boardString += '?';
+                        break;
+                }
+            }
+            boardString += '\n';
+        }
+        return boardString;
+    }
+
+    public boolean isEmpty(int X, int O) {
+        if (array[X][O] != Mark.EMPTY) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public void setMark(int X, int O, Mark mark) {
+        if (isEmpty(X, O)) {
+            array[X][O] = mark;
+        }
+
+    }
+
+    public boolean isWin(Mark mark) {
+        return winConditions(mark);
+    }
+
+    private boolean winConditions(Mark mark) {
+        boolean ifWin = true;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                //X dimension win
+                if (array[j][i] != mark) {
+                    ifWin = false;
+                    break;
+                    //Y dimension win
+                } else if (array[i][j] != mark) {
+                    ifWin = false;
+                    break;
+                    //Angle dimension win
+                } else if (array[i][i] != mark) {
+                    ifWin = false;
+                    break;
+                }
+            }
+        }
+        return ifWin;
+    }
+
+
+    public boolean isFull() {
+        boolean isFull = true;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i][j] == Mark.EMPTY) ;
+                isFull = false;
+                break;
+            }
+        }
+        return isFull;
+    }
 }
